dd874956d5e788ece9dd7743d5225d7b53d1c60b
Sreevathsa <sreevathsa8989@gmail.com>
2024-09-15 00:29:52 +0530

    added login and dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }  // Redirect unknown routes to login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
